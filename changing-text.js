d959e5722afd2fd298cf1d0d6d5c686fad326dcb
chadthedev <chadthedev@gmail.com>
2017-07-14 10:38:46 -0600

    changing-text is now a jquery function

--- changing-text.js
+++ changing-text.js
@@ -1,73 +1,77 @@
-function changingText(stringsToType, options={
-	blinkSpeed : 500,
-	typingSpeed : 50,
-	blinksPerPause : 2
-}){
-	console.log(options.typingSpeed);
-	//change-text is the id of the parent element, changing-text is the child span of change-text
-	$("#change-text").html("<span id='changing-text'></span><span id='blinking-line'>|</span>");
-	var stringIndex = 0; //which string will be typed or erased
-	var activeString = false; //toggles as strings are either typed or erased
-	blinkingLine();
+(function ( $ ){
 
-	function blinkingLine(){ //creates a blinking effect for 2 blinks then calls another function
-		var hidden = false;//toggles as the line blinks
-		var count = 0;
-		var timer = setInterval(blinkingTextLine, options.blinkSpeed);
-		function blinkingTextLine() { 
-			if (hidden) {
-				$("#blinking-line").css("visibility", "visible");
-				hidden = false;
-			}else{
-				$("#blinking-line").css("visibility", "hidden");
-				hidden = true;
-			}
-			if (count > options.blinksPerPause) {
-			 	$("#blinking-line").css("visibility", "visible");
-			 	clearInterval(timer); 
-				activeString ? removeString() : addString();
-			}
-			count++;
-		}
-	}
+	$.fn.changingText = function(stringsToType, options){
+		var settings = $.extend({
+			blinkSpeed : 500,
+			typingSpeed : 50,
+			blinksPerPause : 2,
+		}, options);
+		//change-text is the id of the parent element, changing-text is the child span of change-text
+		$(this).html("<span class='changing-text'></span><span class='blinking-line'>|</span>");
+		var stringIndex = 0; //which string will be typed or erased
+		var activeString = false; //toggles as strings are either typed or erased
+		blinkingLine($(this));
 
-	function addString() { //adds a string in a way that looks like its being typed
-		activeString = true;
-		var count = 0;
-		var string = stringsToType[stringIndex];
-		var timer = setInterval(addChar, options.typingSpeed);
-		function addChar() {
-			if (count > string.length) {
-				clearInterval(timer);
-				blinkingLine();
-			}else{
-				$("#changing-text").append(string[count]);
+		function blinkingLine(jqueryObject){ //creates a blinking effect for 2 blinks then calls another function
+			var hidden = false;//toggles as the line blinks
+			var count = 0;
+			var timer = setInterval(blinkingTextLine, settings.blinkSpeed);
+			function blinkingTextLine() { 
+				if (hidden) {
+					jqueryObject.children(".blinking-line").css("visibility", "visible");
+					hidden = false;
+				}else{
+					jqueryObject.children(".blinking-line").css("visibility", "hidden");
+					hidden = true;
+				}
+				if (count > settings.blinksPerPause) {
+				 	jqueryObject.children(".blinking-line").css("visibility", "visible");
+				 	clearInterval(timer); 
+					activeString ? removeString(jqueryObject) : addString(jqueryObject);
+				}
 				count++;
 			}
 		}
-	}
 
-	function removeString() {// removes a string in a way that looks like its being typed
-		activeString = false;
-		var count = 0;
-		var string = stringsToType[stringIndex];
-		var timer = setInterval(removeChar, options.typingSpeed);
-		function removeChar() {
-			if (count > string.length) {
-				clearInterval(timer);
-				nextString();
-			}else{
-				count++;
-				$("#changing-text").text(string.slice(0, -count));
+		function addString(jqueryObject) { //adds a string in a way that looks like its being typed
+			activeString = true;
+			var count = 0;
+			var string = stringsToType[stringIndex];
+			var timer = setInterval(addChar, settings.typingSpeed);
+			function addChar() {
+				if (count > string.length) {
+					clearInterval(timer);
+					blinkingLine(jqueryObject);
+				}else{
+					jqueryObject.children(".changing-text").append(string[count]);
+					count++;
+				}
 			}
 		}
-	}
 
-	function nextString(){ //either adds to string index or puts string index back to 0
-		stringIndex++;
-		if (stringIndex >= stringsToType.length) {
-			stringIndex = 0;
+		function removeString(jqueryObject) {// removes a string in a way that looks like its being typed
+			activeString = false;
+			var count = 0;
+			var string = stringsToType[stringIndex];
+			var timer = setInterval(removeChar, settings.typingSpeed);
+			function removeChar() {
+				if (count > string.length) {
+					clearInterval(timer);
+					nextString(jqueryObject);
+				}else{
+					count++;
+					jqueryObject.children(".changing-text").text(string.slice(0, -count));
+				}
+			}
 		}
-		blinkingLine();
-	}
-}
+
+		function nextString(jqueryObject){ //either adds to string index or puts string index back to 0
+			stringIndex++;
+			if (stringIndex >= stringsToType.length) {
+				stringIndex = 0;
+			}
+			blinkingLine(jqueryObject);
+		}
+	};
+
+}(jQuery));
